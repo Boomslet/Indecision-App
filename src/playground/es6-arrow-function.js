f0408511326e154f0229647dc72d8f89a0c026bf
Mark Boon <Boomslet@users.noreply.github.com>
2018-01-10 15:25:56 +0800

    Add files via upload

---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,17 @@
+// const square = function(x) {
+//     return x*x;
+// };
+
+// // const squareArrow = (x) => {
+// //     return x*x;
+// // };
+
+// const squareArrow = (x) => x*x;
+
+const getFirstName = (name) => {
+    return name.split(' ')[0];
+};
+
+const getFirstName2 = (name) => name.split(' ')[0];
+
+console.log(getFirstName2('Mark Boon'));
