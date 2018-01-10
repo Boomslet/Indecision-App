3fcf534b305f127375437b82a578e386e5fde5eb
Mark Boon <Boomslet@users.noreply.github.com>
2018-01-10 23:11:42 +0800

    Add files via upload

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -5,7 +5,7 @@ class Person {
     }
 
     getGreeting() {
-        return `Hi. I am ${this.name}`;
+        return `Hi. I am ${this.name}.`;
     }
 
     getDescription() {
@@ -13,8 +13,45 @@ class Person {
     }
 }
 
-const me = new Person('Mark Boon', 24);
-const you = new Person();
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
 
-console.log(me.getDescription());
-console.log(you.getDescription());
+    hasMajor() {
+        return !!this.major;
+    }
+
+    getDescription() {
+        let description = super.getDescription();
+
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}`
+        }
+        return description;
+    }
+}
+
+class Traveller extends Person {
+    constructor(name, age, home) {
+        super(name, age);
+        this.home = home;
+    }
+
+    getGreeting() {
+        let greeting = super.getGreeting();
+
+        if (this.home) {
+            greeting += ` I'm visiting from ${this.home}`;
+        }
+
+        return greeting;
+    }
+}
+
+const me = new Traveller('Mark Boon', 24, 'Australia');
+const you = new Traveller();
+
+console.log(me.getGreeting());
+console.log(you.getGreeting());
