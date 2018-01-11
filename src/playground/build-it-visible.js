50546aafbf4610d74294f7b8d18635c278a46a49
Mark Boon <Boomslet@users.noreply.github.com>
2018-01-11 12:54:35 +0800

    Add files via upload

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,24 +1,58 @@
-let hidden = true;
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibilty = this.handleToggleVisibilty.bind(this);
+        this.state = {
+            visibility: false
+        };
+    }
 
-const toggleHidden = () => {
-    hidden = !hidden;
-    render();
-};
-
-const render = () => {
-    const template = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={toggleHidden}>
-                {hidden ? 'Show details': 'Hide details'}
-            </button>
-            {
-                !hidden && <p>Hey. These are some details you can now see!</p>
+    handleToggleVisibilty () {
+        this.setState((prevState) => {
+            return  {
+                visibility: !prevState.visibility
             }
-        </div>
-    );
-    
-    ReactDOM.render(template, document.getElementById("app"));
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Visibilty Toggle</h1>
+                <button onClick={this.handleToggleVisibilty}>
+                    {this.state.visibility ? 'Hide details' : 'Show details'}
+                </button>
+                {
+                    this.state.visibility && <p>Hey. These are some details you can now see!</p>
+                }
+            </div>
+        );
+    }
 }
 
-render();
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
+
+// let hidden = true;
+
+// const toggleHidden = () => {
+//     hidden = !hidden;
+//     render();
+// };
+
+// const render = () => {
+//     const template = (
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <button onClick={toggleHidden}>
+//                 {hidden ? 'Show details': 'Hide details'}
+//             </button>
+//             {
+//                 !hidden && <p>Hey. These are some details you can now see!</p>
+//             }
+//         </div>
+//     );
+    
+//     ReactDOM.render(template, document.getElementById("app"));
+// }
+
+// render();
